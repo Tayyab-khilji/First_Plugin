bed44c5416673eab41963455c515607d927ad017
Tayyab-khilji <71983452+Tayyab-khilji@users.noreply.github.com>
2021-01-18 15:49:04 +0500

    Update first.php

--- first.php
+++ first.php
@@ -7,4 +7,13 @@ Version: 1.0
 Author: Tayyab Rehman
 Author URI: 
 */
+add_action("admin_menu", "addMenu");
+function addMenu()
+{
+ add_menu_page("Plugin Settings", "Plugin Settings", 4, "Plugin  Settings", "pluginSetting");
+}
+function pluginSetting()
+{
+    echo "hello World";
+}
 ?>
